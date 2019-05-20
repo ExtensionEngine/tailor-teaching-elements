9c723703c4d4e2ffc23433857b10434c861759d9
Dario Vladovic <d.vladimyr@gmail.com>
2019-05-20 23:15:35 +0200

    Cleanup :lipstick:

--- config/bili.config.js
+++ config/bili.config.js
@@ -1,20 +1,42 @@
+'use strict';
+
 const path = require('path');
 
-const sourcemap = true;
+const filename = 'tailor-teaching-elements';
+const moduleName = 'TailorTeachingElements';
+const sourceDir = path.resolve(__dirname, '../src');
+const sourceMap = true;
 
-// open flag blocks build process
+const babel = {
+  babelrc: false,
+  runtimeHelpers: true,
+  sourceMap
+};
+
+const alias = {
+  '@': sourceDir
+};
+
+// NOTE: ``--open'' flag blocks build process
 // https://github.com/btd/rollup-plugin-visualizer/issues/27
-const visualizer = { sourcemap };
-const alias = { '@': path.resolve(__dirname, '../src') };
+const visualizer = {
+  sourcemap: sourceMap
+};
 
+/** @type {import('bili').Config} */
 module.exports = {
-  babel: { babelrc: false },
-  plugins: { vue: true, 'node-builtins': true, alias, visualizer },
+  input: { [filename]: sourceDir },
+  plugins: {
+    'node-builtins': true,
+    vue: true,
+    babel,
+    alias,
+    visualizer
+  },
   output: {
     extractCSS: false,
     format: ['esm', 'umd', 'umd-min'],
-    fileName: 'tailor-teaching-elements[min].[format][ext]',
-    moduleName: 'TailorTeachingElements',
-    sourceMap: sourcemap
+    moduleName,
+    sourceMap
   }
 };
