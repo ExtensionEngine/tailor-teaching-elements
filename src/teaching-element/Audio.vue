ae82f6f51a237f027fbe6201366d441e621e4d0a
Zdravko Ćurić <zcuric@gmail.com>
2019-10-09 11:29:30 +0200

    Implement te-audio

---
+++ src/teaching-element/Audio.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="te-audio">
+    <aplayer
+      v-if="url"
+      v-bind="playerOptions"
+      :music="music" />
+  </div>
+</template>
+
+<script>
+import Aplayer from 'vue-aplayer';
+Aplayer.disableVersionBadge = true;
+
+const defaultAudioOptions = {
+  title: 'Audio track',
+  artist: ' ',
+  pic: ' '
+};
+
+export default {
+  name: 'te-file',
+  inheritAttrs: false,
+  props: {
+    url: { type: String, required: true },
+    audioOptions: { type: Object, default: () => defaultAudioOptions },
+    playerOptions: { type: Object, default: () => ({}) }
+  },
+  computed: {
+    music() {
+      return {
+        url: this.url,
+        ...this.audioOptions
+      };
+    }
+  },
+  components: {
+    Aplayer
+  }
+};
+</script>
