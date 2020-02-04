9cfb8d46477a69be50671ae835ce9ee5fc9ebd97
abasic <antoniobasic@gmail.com>
2020-02-05 00:23:34 +0100

    Fixed fill in the blank reflection:
     - extacted question parsing to root assessment component
     - now tracks blank count per HTML element
     - now uses that count instead of correct length to render inputs
     - minor refactor

--- src/teaching-element/Assessment/Question.vue
+++ src/teaching-element/Assessment/Question.vue
@@ -4,7 +4,7 @@
     <div class="question">
       <div class="row">
         <primitive
-          v-for="{ id, data, type } in parsedQuestion"
+          v-for="{ id, data, type } in question"
           :key="id"
           v-bind="data"
           :type="type" />
@@ -16,30 +16,10 @@
 <script>
 import Primitive from '../Primitive.vue';
 
-function buildBlankReplacer() {
-  let counter = 1;
-  return () => `<span class='blank'>
-                  <span>${counter++}</span>
-                  <span class="blank-line"></span>
-                </span>`;
-}
-
 export default {
   name: 'te-question',
   props: {
-    assessmentType: { type: String, required: true },
     question: { type: Array, required: true }
-  },
-  computed: {
-    parsedQuestion() {
-      if (this.assessmentType !== 'FB') return this.question;
-      const blankReplacer = buildBlankReplacer();
-      return this.question.map(({ id, data, type }) => {
-        if (!type.includes('HTML')) return { id, data, type };
-        const content = data.content.replace(/@blank/g, blankReplacer);
-        return { id, data: { ...data, content }, type };
-      });
-    }
   },
   components: { Primitive }
 };
