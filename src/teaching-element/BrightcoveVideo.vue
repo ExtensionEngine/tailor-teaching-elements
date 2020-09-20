73eee3b5c3c4c2bf40e25a321db5302e13a32a98
Vedran Karacic <karacicvedran@gmail.com>
2020-09-20 13:32:18 +0200

    Use brightcove player loader package for the BrightcoveVideo component
    The @brightcove/player-loader package covers all the current
    functionality and fixes the bugs with scripts and style removal.

--- src/teaching-element/BrightcoveVideo.vue
+++ src/teaching-element/BrightcoveVideo.vue
@@ -1,158 +1,54 @@
 <template>
   <div class="te-brightcove-video">
-    <div v-if="showError" class="error">
+    <div v-if="error" class="error">
       <div class="message">
         <span class="alert-icon"></span>
-        <p>Error loading media!</p>
+        <p>Error loading media: {{ error }}</p>
       </div>
     </div>
-    <div v-show="!showError" ref="videoWrapper" class="wrapper"></div>
+    <div v-else ref="videoWrapper"></div>
   </div>
 </template>
 
 <script>
-import join from 'url-join';
-
-const BASE_URL = '//players.brightcove.net/';
-const ERROR_NOT_FOUND = 'VIDEO_CLOUD_ERR_VIDEO_NOT_FOUND';
-const ERROR_INVALID_CONFIG = 'ERR_INVALID_CONFIGURATION';
+import brightcovePlayerLoader from '@brightcove/player-loader';
 
 export default {
   name: 'te-brightcove-video',
   props: {
     accountId: { type: String, required: true },
     playerId: { type: String, required: true },
-    videoId: { type: String, required: true },
-    debug: { type: Boolean, default: false },
-    analytics: { type: Boolean, default: false }
+    videoId: { type: String, required: true }
   },
   data() {
     return {
-      error: null,
-      style: null,
-      script: null
+      error: null
     };
-  },
-  computed: {
-    playerUrl() {
-      const script = this.debug ? 'index.js' : 'index.min.js';
-      return join(BASE_URL, this.accountId, `${this.playerId}_default`, script);
-    },
-    showError() {
-      if (!this.error) return false;
-      const { code } = this.error;
-      return code === ERROR_NOT_FOUND || code === ERROR_INVALID_CONFIG;
-    }
   },
   methods: {
     createPlayer() {
-      const video = document.createElement('video');
-      Object.assign(video.dataset, {
-        account: this.accountId,
-        player: this.playerId,
-        videoId: this.videoId
+      brightcovePlayerLoader({
+        refNode: this.$refs.videoWrapper,
+        accountId: this.accountId,
+        playerId: this.playerId,
+        videoId: this.videoId,
+        onFailure: err => (this.error = err.message)
       });
-      video.className = 'video-js';
-      video.setAttribute('controls', '');
-      return video;
-    },
-    destroyPlayer() {
-      // Dispose player.
-      if (this.player) {
-        this.player.pause();
-        this.player.dispose();
-        this.player = null;
-      }
-      // Clear errors.
-      this.error = null;
-      // Remove player assets.
-      if (this.script) this.$el.removeChild(this.script);
-      this.script = null;
-      if (this.style) document.head.removeChild(this.style);
-      this.style = null;
-      // Wipe wrapper contents.
-      empty(this.$refs.videoWrapper);
-    },
-    initPlayer(playerUrl = this.playerUrl) {
-      // Cleanup previous instance.
-      this.destroyPlayer();
-      // Load player script.
-      this.script = loadScript(playerUrl, this.$el, err => {
-        if (err) {
-          this.onError({
-            code: ERROR_INVALID_CONFIG,
-            accountId: this.accountId,
-            playerId: this.playerId
-          });
-          return;
-        }
-        // Override default videojs styling.
-        this.style = this.setTheme();
-        // Create new video element.
-        const video = this.createPlayer();
-        this.$refs.videoWrapper.appendChild(video);
-        // Initialize Brightcove player using bc API.
-        if (!this.analytics) disableAnalytics(window.bc.videojs);
-        this.player = window.bc(video);
-        this.player.autoplay(false);
-        this.player.on('error', () => this.onError(this.player.error()));
-      });
-    },
-    setTheme() {
-      const cls = `bc-style-${this.playerId}-default`;
-      const theme = document.head.getElementsByClassName(cls)[0];
-      return document.head.insertBefore(theme, null);
-    },
-    pause() {
-      if (this.player) this.player.pause();
-    },
-    onError(err) {
-      this.error = err;
     }
   },
   watch: {
-    playerUrl() {
-      if (!this.$el) return;
-      this.initPlayer();
-    },
     videoId() {
-      if (!this.$el) return;
-      this.initPlayer();
+      brightcovePlayerLoader.reset();
+      this.createPlayer();
     }
   },
   mounted() {
-    this.initPlayer();
+    this.createPlayer();
   },
   beforeDestroy() {
-    this.destroyPlayer();
+    brightcovePlayerLoader.reset();
   }
 };
-
-function disableAnalytics(videojs) {
-  // Swapping `bcAnalytics` with stub developed according to:
-  // http://players.brightcove.net/1752604059001/VJCJXL3Ye_default/index.js
-  const registerPlugin = videojs.registerPlugin || videojs.plugin;
-  registerPlugin.call(videojs, 'analyticsBlocker', function init() {
-    this.bcAnalytics = { client: { getSessionId } };
-  });
-
-  function getSessionId() {
-    return (Math.random() * 0xFFFFFFFF >>> 0).toString(16);
-  }
-}
-
-function loadScript(url, dest, cb) {
-  const script = document.createElement('script');
-  script.src = url;
-  script.async = false;
-  script.onload = () => cb(null, script);
-  script.onerror = () => cb(new Error('Error loading script!'), script);
-  return dest.appendChild(script);
-}
-
-function empty(el) {
-  if (el) el.innerHTML = '';
-}
 </script>
 
 <style lang="scss" scoped>
