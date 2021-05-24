38210ff0ada17de2574cef612d20b33a0e4a66b6
Marin Capeta <marin.capeta@gmail.com>
2021-05-24 11:59:41 +0200

    Add `alt` and `caption` to the image element

--- src/teaching-element/Image.vue
+++ src/teaching-element/Image.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="te-image">
-    <img :src="url">
-  </div>
+  <figure class="te-image">
+    <img :src="url" :alt="alt" :title="alt">
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <figcaption v-if="caption" v-html="caption"></figcaption>
+  </figure>
 </template>
 
 <script>
 export default {
   name: 'te-image',
   props: {
-    url: { type: String, required: true }
+    url: { type: String, required: true },
+    alt: { type: String, default: '' },
+    caption: { type: String, default: '' }
   }
 };
 </script>
 
 <style lang="scss">
-.te-image img {
-  display: block;
-  max-width: 100%;
-  height: auto;
-  margin: 0 auto;
+.te-image {
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    margin: 0 auto;
+  }
+
+  figcaption {
+    text-align: center;
+  }
 }
 </style>
